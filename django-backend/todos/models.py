4d3d4d069bff5c953139b626becd4a08817d679a
Muturi David <muturidavid854@gmail.com>
2025-09-08 02:42:57 +0300

    update model

--- django-backend/todos/models.py
+++ django-backend/todos/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Todo(models.Model):
-    title = models.CharField(max_length=200)
+    task = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- django-backend/todos/serializers.py
+++ django-backend/todos/serializers.py
@@ -4,4 +4,4 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'completed', 'created_at', 'updated_at']
+        fields = ['id', 'task', 'completed', 'created_at', 'updated_at']
